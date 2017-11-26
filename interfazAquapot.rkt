7fad43f5b04a3bfd5eb361f34049dbf7a1fcceb6
OscarordonezM <33988850+OscarordonezM@users.noreply.github.com>
2017-11-25 19:54:09 -0500

    Update interfazAquapot.rkt

--- interfazAquapot.rkt
+++ interfazAquapot.rkt
@@ -1,44 +1,41 @@
 #lang racket
 (require racket/gui)
-#|
-(define ventana (new frame% [label "hola mundo"]))
-#|(define mensaje (new message% [parent ventana]
-                     [label "bla bla bla bla"]))|#
-#(send ventana show #t)
 
-
-
-(define (funcion-manejadora b c)
-  (if (object=? b boton1)
-      (send mensaje set-label "se presiono el boton 1")
-      (send mensaje set-label "se presiono el boton 2")
-      )
-  )
-
-(define boton1 (new button%
-                    [parent ventana]
-                    [label "boton 1"]
-                    [callback funcion-manejadora]
-                    ))
-(define mensaje (new message% [parent ventana]
-                     [label "aun no se a presionadi ningun boton"]))
-(define boton2 (new button%
-                    [parent ventana]
-                    [label "boton 2"]
-                    [callback funcion-manejadora]
-                    ))
-(send ventana show #t)
-|#
 ;=============================VENTANA PRINCIPAL===========================
 
-(define ventana (new frame% [label "AQUABOT"]))
+(define ventana (new frame%
+                     [label "AQUABOT"]
+                     [width 400]
+                     [height 100]
+                     ))
 (define datos (new message%
                      [parent ventana]
                      [label ""]
                      [auto-resize #t]
                      ))
+
+(define panel-botones-ventana (new horizontal-pane% [parent ventana]))
+(define botonSeleccionar (new button%
+                              [parent panel-botones-ventana]
+                              [label "SELECCIONAR USUARIO"]
+                              [callback (lambda (b c)
+                                          (send ventana-seleccionar show #t))]))
+
+(define boton-Registrar
+  (new button%
+       [parent panel-botones-ventana]
+       [label "REGISTRAR USUARIO"]
+       [callback
+        (lambda (b c)
+          (send ventana-registro show #t)
+          )
+        ])
+
+  )
+;===========================VENTANA REGISTRO=============================
 (define ventana-registro (new dialog%
                               [label "REGISTRO USUARIO"]))
+;(define panel-registro (new vertical-pane%) [parent ventana-registro])
 
 (define txt-nombre (new text-field%
                          [label "Nombre :"]
@@ -54,85 +51,6 @@
                           [label "Fecha Nacimiento :"]
                           [parent ventana-registro]
                           ))
-
-;============================VENTANA SELECCIONAR USUARIO=================
-
-(define ventana-seleccionar (new dialog%
-                                 [label "SELECCIONAR USUARIO"]))
-
-(define (usuario r c)     ;instancia la lista y control-evento
-  (send mensaje set-label
-        (string-append "opcion lista"
-                       (number->string (send r get-selection))
-                       "con texto"
-                       (send r get-string-selection)
-                       "de la lista"
-                       (let ([t (send r get-label)])
-                         (if t t "<sin-texto>"))
-                       "'"
-                       ))
-  )
-(define choice-seleccionar-usuario (new choice%
-                      [label "Usuario"]
-                      [choices (list "usuario 1" "usuario 2")]
-                      [parent ventana-seleccionar]
-                      [callback usuario]
-                      ))
-(define botonSeleccionar (new button%
-                              [parent ventana]
-                              [label "SELECCIONAR USUARIO"]
-                              [callback (lambda (b c)
-                                          (send ventana-seleccionar show #t))]))
-(define botonMostrar (new button%
-                          [parent ventana-seleccionar]
-                          [label "MOSTRAR"]
-                          [callback (lambda (b c)
-                                      (send ventana-registro show #t))]));llamar mostrar informacion pendiente
-(define botonGenerarRecibo (new button%
-                                [parent ventana-seleccionar]
-                                [label "GENERAR RECIBO"]
-                                [callback (lambda (b c)
-                                            (send ventana-registro show #t))]) );llamar generar recibo pendiente
-(define botonCalcular (new button%
-                           [parent ventana-seleccionar]
-                           [label "CALCULAR"]
-                           [callback (lambda (b c)
-                                       (send ventana-registro show #t))])); llamar a calcular PENDIENTE!!!
-(define ventana-calcular (new dialog%
-                              [label "CALCULAR"]))
-
-(define ventana-recibo (new dialog%
-                            [label "RECIBO"]))
-
-(define Registrar
-  (new button%
-       [parent ventana]
-       [label "REGISTRAR USUARIO"]
-       [callback
-        (lambda (b c)
-          (send ventana-registro show #t)
-          #|(send datos set-label
-                (string-append "Nombre : '"
-                               (send txt-nombre get-value)
-                               "', Apellido :'"
-                               (send txt-apellido get-value)
-                               "', Fecha Nacimiento :'"
-                               (send txt-fechaNacimiento get-value)
-                               "',Genero :'"
-                               (send radio-Genero get-value)
-                               "',Estrato :"
-                               (send choice-estrato get-value)
-                               ""
-                               (send choice-nivelEstudio get-value)
-                               ""
-                               )
-                )|#)
-        ])
-
-  )
-
-
-
 (define (Genero r c)
   (send mensaje set-label
         (string-append "se marco la opcion"
@@ -145,18 +63,14 @@
                        "'"))
   )
 
-(define (Estrato r c)     ;instancia la lista y control-evento
-  (send mensaje set-label
-        (string-append "opcion lista"
-                       (number->string (send r get-selection))
-                       "con texto"
-                       (send r get-string-selection)
-                       "de la lista"
-                       (let ([t (send r get-label)])
-                         (if t t "<sin-texto>"))
-                       "'"
-                       ))
-  )
+(define radio-Genero (new radio-box%
+                    [label "Genero "]
+                    [parent ventana-registro]
+                    [choices (list "Masculino" "Femenino")]
+                    [callback Genero]
+                    ))
+
+
 
 (define (nivelEstudio r c)
   (send mensaje set-label
@@ -177,6 +91,19 @@
                           [parent ventana-registro]
                           [callback nivelEstudio]))
 
+(define (Estrato r c)     ;instancia la lista y control-evento
+  (send mensaje set-label
+        (string-append "opcion lista"
+                       (number->string (send r get-selection))
+                       "con texto"
+                       (send r get-string-selection)
+                       "de la lista"
+                       (let ([t (send r get-label)])
+                         (if t t "<sin-texto>"))
+                       "'"
+                       ))
+  )
+
 (define choice-estrato (new choice%
                       [label "Estrato "]
                       [choices (list "1" "2" "3" "4" "5" "6")]
@@ -184,12 +111,65 @@
                       [callback Estrato]
                       ))
 
-(define radio-Genero (new radio-box%
-                    [label "Genero "]
-                    [parent ventana-registro]
-                    [choices (list "Masculino" "Femenino")]
-                    [callback Genero]
-                    ))
+(define botonRegistrar(new button%
+                           [parent ventana-registro]
+                           [label "REGISTRAR"]
+                           [callback (lambda ( b c)
+                                       (send ventana-registro show #t))]))
+
+;============================VENTANA SELECCIONAR USUARIO=================
+
+(define ventana-seleccionar (new dialog%
+                                 [label "SELECCIONAR USUARIO"]))
+
+(define panel-opciones-horizontal (new horizontal-pane% [parent ventana-seleccionar]))
+(define panel-opciones-vertical (new vertical-pane% [parent ventana-seleccionar]))
+(define (usuario r c)     ;instancia la lista y control-evento
+  (send mensaje set-label
+        (string-append "opcion lista"
+                       (number->string (send r get-selection))
+                       "con texto"
+                       (send r get-string-selection)
+                       "de la lista"
+                       (let ([t (send r get-label)])
+                         (if t t "<sin-texto>"))
+                       "'"
+                       ))
+  )
+(define choice-seleccionar-usuario (new choice%
+                      [label "Usuario"]
+                      [choices (list "usuario 1" "usuario 2")]
+                      [parent panel-opciones-horizontal]
+                      [callback usuario]
+                      ))
+
+(define botonMostrar (new button%
+                          [parent panel-opciones-horizontal]
+                          [label "MOSTRAR"]
+                          [callback (lambda (b c)
+                                      (send ventana-registro show #t))]));llamar mostrar informacion pendiente
+(define botonGenerarRecibo (new button%
+                                [parent panel-opciones-vertical]
+                                [label "GENERAR RECIBO"]
+                                [callback (lambda (b c)
+                                            (send ventana-registro show #t))]) );llamar generar recibo pendiente
+(define botonCalcular (new button%
+                           [parent panel-opciones-vertical]
+                           [label "CALCULAR"]
+                           [callback (lambda (b c)
+                                       (send ventana-registro show #t))])); llamar a calcular PENDIENTE!!!
+(define ventana-calcular (new dialog%
+                              [label "CALCULAR"]))
+
+(define ventana-recibo (new dialog%
+                            [label "RECIBO"]))
+
+
+
+
+
+
+
 
 (define mensaje (new message%
                      [label ""]
@@ -197,11 +177,7 @@
                      [auto-resize #t]
                      ))
 
-(define botonRegistrar(new button%
-                           [parent ventana-registro]
-                           [label "REGISTRAR"]
-                           [callback (lambda ( b c)
-                                       (send ventana-registro show #t))]))
+
 
 
 #|(define panel (new horizontal-panel% [parent ventana]))
